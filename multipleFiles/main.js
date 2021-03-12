34248af195c3b9c8fd573c64b75a1488e8567075
David Lukens <davidqcy02@gmail.com>
2021-03-11 20:54:48 -0600

    finishVideoOnJS

---
+++ multipleFiles/main.js
@@ -0,0 +1,7 @@
+
+//import { capitalizeString}  from "./stringFunctions.js"
+import * as stringFunctions from "./stringFunctions.js";
+
+const cap = stringFunctions.capitalizeString("using multiplefiles")
+
+console.log(cap);
